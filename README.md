74f9da4519145d60eb141e455fc5a7bba489b84e
dzsuhesz <grperge@gmail.com>
2025-12-05 15:29:00 +0100

    Added image and text for button status change with basic readme changes the image addition

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This a simple "Hello World" level React application for a DEVOPS projekt.
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import logo from './ok-cat.gif';
 import { useState } from 'react';
 
 function App() {
@@ -8,6 +9,13 @@ function App() {
         <button onClick={() => setShowImage(!showImage)}>
           {showImage ? "You actually dared" : "I dare you"}
         </button>
+        {showImage && (
+          <div>
+            <img src={logo} alt="the coolest cat" width="500"></img>
+            <h1>Damn you are cool</h1>
+          </div>
+        )}
+        
     </div>
   );
 }
