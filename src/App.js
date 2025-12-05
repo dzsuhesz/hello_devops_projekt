0d071168a758037c8895a9e0bc2d543001c7874d
dzsuhesz <grperge@gmail.com>
2025-12-05 15:23:38 +0100

    Added button with useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [showImage, setShowImage] = useState(false);
   return (
     <div style={{ textAlign: "center"}}>
         <h1> Don't you dare click on this. </h1>
+        <button onClick={() => setShowImage(!showImage)}>
+          {showImage ? "You actually dared" : "I dare you"}
+        </button>
     </div>
   );
 }
-
 export default App;
